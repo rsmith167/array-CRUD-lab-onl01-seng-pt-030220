74776e7ab0910af58a522d97af153588af2477f5
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 12:35:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  aNewerArray = ["fish", "deer"]
+  aNewerArray = ["fish", "deer","october","octopus"]
 end
 
 def add_element_to_end_of_array(array, element)
